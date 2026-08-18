0a44cf47e75d4e3af59c23a7171f641114a691e1
Michael Spangler <ms@michaelspangler.io>
2026-08-18 14:37:26 +0000

    delete functions.c and .h because I don't need them yet

--- src/functions.c
+++
@@ -1,6 +0,0 @@
-#include "functions.h"
-
-void
-delete_word(buffer *b, u64 offset)
-{
-}
--- src/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-#include "base.h"
-#include "buffer.h"
-
-void delete_word(buffer *b, u64 offset);
-
-#endif
